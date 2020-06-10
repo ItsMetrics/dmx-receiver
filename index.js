7149f91d18d009eb06f65e7f0f865f108f70a4ce
Jason Segarra <youdontknowthequestion@gmail.com>
2020-06-10 11:23:09 -0500

    read config file for settings

--- index.js
+++ index.js
@@ -3,6 +3,11 @@
 const receiver = require('./source/receiver');
 const controller = require('./source/dmx-controller');
 
+const fs = require('fs');
+// read the config file
+var configData = JSON.parse(fs.readFile('./config/config.json'));
+
+
 const Receiver = new receiver({port: 17809, address: 'http://localhost:'});
 //const Controller = new controller({useNullDevice: true});
 //const Controller = new controller({device: '/dev/ttyUSB0', deviceOptions: {}});
