fee0398bb3cb57b50913022415dfacb206d20bb1
Jason Segarra <youdontknowthequestion@gmail.com>
2020-06-10 11:31:39 -0500

    adding receiver options into the config file

--- index.js
+++ index.js
@@ -5,10 +5,9 @@ const controller = require('./source/dmx-controller');
 
 const fs = require('fs');
 // read the config file
-var configData = JSON.parse(fs.readFile('./config/config.json'));
+var configData = JSON.parse(fs.readFileSync('./config/config.json', 'utf8'));
 
-
-const Receiver = new receiver({port: 17809, address: 'http://localhost:'});
+const Receiver = new receiver({port : configData.receiver.port, address : configData.receiver.host});
 //const Controller = new controller({useNullDevice: true});
 //const Controller = new controller({device: '/dev/ttyUSB0', deviceOptions: {}});
 
